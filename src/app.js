442a43f158e55737de1cd7e883a2ae199ca3cd9b
S-Riches <samuelrichesprivate@gmail.com>
2023-02-04 23:57:08 +0000

    did some tidying

--- src/app.js
+++ src/app.js
@@ -4,17 +4,24 @@ const dotenv = require("dotenv").config();
 
 // grab the discord token from the .env file
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-
+// allows reuse
+// create a new instance of the client, returns the client once created to check the client was created successfully
 function createClientInstance() {
-    // create a new instance of the client
     // intents define which events discord sends to the bot, it ensures the caches for servers, channels and roles are available to use
     const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
     // when the client is ready, run this code
     client.once(Events.ClientReady, (c) => {
         console.log(`Logged in as ${c.user.tag}`);
     });
-
-    // log in to discord with the token
     client.login(DISCORD_TOKEN);
 }
+
+module.exports = { createClientInstance };
+
+// client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// // when the client is ready, run this code
+// client.once(Events.ClientReady, (c) => {
+//     console.log(`Logged in as ${c.user.tag}`);
+// });
+
+createClientInstance();
--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const app = require("../src/app.js");
 const dotenv = require("dotenv").config();
 
-describe("Set up initialisation", () => {
+describe("Enviroment variable check test suite", () => {
     test("env values are working", () => {
         const osEnvValue = process.env.os;
         expect(osEnvValue).not.toBeUndefined();
@@ -10,4 +10,8 @@ describe("Set up initialisation", () => {
         const TOKEN = process.env.DISCORD_TOKEN;
         expect(TOKEN).not.toBeUndefined();
     });
+});
+
+describe("Discord bot test suite", () => {
+    // not terribly sure how to test the bot, need to read up on this as it makes the most sense to test commands??
 });
